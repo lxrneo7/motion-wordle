221754365ac303da02e2da7a8c3058745cdeefb2
darknes7sjdhfouef <eshimbekovabdutahim@gmail.com>
2024-05-04 17:42:33 +0600

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header>
+        <div className="container">
+            <div className="header">
+                header
+            </div>
+        </div>
+    </header>
+  )
+}
