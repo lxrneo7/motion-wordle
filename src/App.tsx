0f49f5a0fde1a17ac4e5e2dfcb3d0e1390ab8fef
darknes7sjdhfouef <eshimbekovabdutahim@gmail.com>
2024-05-13 18:02:31 +0600

    Добавлено поведение клавиатуры аналогичное игре Wordle в компоненте Board

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import Board from "./components/Board";
 import "./App.css";
+import useFetchRandomWord from "./hooks/useFetchRandomWord";
 
 export default function App() {
   const [tiles, setTiles] = useState<string[]>(Array(30).fill(""));
+  const randomWord = useFetchRandomWord();
 
-  useEffect(() => {
-    const fetchRandomWord = async () => {
-      try {
-        const response = await fetch("https://piccolo-server.vercel.app/words");
-        const data = await response.json();
-        const words: string[] = data.data;
-        const randomIndex: number = Math.floor(Math.random() * words.length);
-        const randomWord: string = words[randomIndex];
-        updateTiles(randomWord);
-      } catch (error) {
-        console.error("Error fetching random word:", error);
-      }
-    };
-
-    fetchRandomWord();
-  }, []); 
-
-  const updateTiles = (word: string) => {
-    const newTiles: string[] = [...word.toUpperCase().split(""), ...Array(30 - word.length).fill("")];
-    setTiles(newTiles);
-  };
 
   const handleTileChange = (index: number, value: string) => {
     const newTiles: string[] = [...tiles];
     newTiles[index] = value.toUpperCase(); 
     setTiles(newTiles);
   };
+
+  React.useEffect(() => {
+    if (randomWord) {
+      setTiles([...randomWord.split(""), ...Array(30 - randomWord.length).fill("")]);
+    }
+  }, [randomWord]);
 
   return (
     <div className="App">
